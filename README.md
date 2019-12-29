c0f0111d4d34b6b1d40b21536d7007139091bf0e
Simon Bracegirdle <sbracegirdle@gmail.com>
2019-12-29 23:16:13 +0800

    Add AWS CDK infrastructure scripts. Update README

--- README.md
+++ README.md
@@ -1,8 +1,47 @@
 # Simon's Personal Site
 
-This is the code to my personal site. 99% static html/css based on the bootstrap framework and using the [Freelancer theme](https://github.com/BlackrockDigital/startbootstrap-freelancer)
+This is the source code to my personal site; [bracegirdle.me](https://bracegirdle.me).
+
+Tech stack:
+
+* Static html/css based on the Bootstrap framework and using the [Freelancer theme](https://github.com/BlackrockDigital/startbootstrap-freelancer).
+* Hosted on AWS S3
+* Distributed via AWS CloudFront CDN
+* Certificates provided by AWS ACM
+* DNS provided by AWS Route 53
+
+Structure:
+
+* `src` HTML/CSS source code for webpage
+* `infra` CDK source code for AWS resources
+* `freelancer-theme` Customised Freelancer Bootstrap theme.
+
+
+## Deployment
+
+Assuming AWS CLI and CDK is setup:
+
+```s
+cd infra
+cdk deploy
+```
+
+## Updating theme CSS
+
+e.g. for tweaking theme variables etc
+
+```s
+cd freelancer-theme
+npm i
+npm run css
+```
+
+(runs gulp CSS)
 
 
 ## License note
 
-Please refer to the [LICENSE](https://bitbucket.org/si13b/personal-site/src/theme/LICENSE) for Freelancer theme MIT license.
+Please refer to the following licenses:
+
+* Simon's personal site [MIT license](https://bitbucket.org/si13b/personal-site/src/master/LICENSE)
+* Freelancer bootstrap theme [MIT license](https://bitbucket.org/si13b/personal-site/src/master/freelancer-theme/LICENSE) for .
---
+++ infra/infra.js
@@ -0,0 +1,103 @@
+#!/usr/bin/env node
+
+const cdk = require("@aws-cdk/core");
+const cloudfront = require("@aws-cdk/aws-cloudfront");
+const route53 = require("@aws-cdk/aws-route53");
+const s3 = require("@aws-cdk/aws-s3");
+const s3deploy = require("@aws-cdk/aws-s3-deployment");
+const acm = require("@aws-cdk/aws-certificatemanager");
+const targets = require("@aws-cdk/aws-route53-targets/lib");
+
+class PersonalSiteStack extends cdk.Stack {
+  constructor(app, id, props) {
+    super(app, id, props);
+
+    const siteDomain = props.domainName;
+    const zone = route53.HostedZone.fromLookup(this, "Zone", {
+      domainName: siteDomain
+    });
+    new cdk.CfnOutput(this, "Site", { value: "https://" + siteDomain });
+
+    const siteBucket = new s3.Bucket(this, "SiteBucket", {
+      bucketName: siteDomain,
+      websiteIndexDocument: "index.html",
+      websiteErrorDocument: "error.html",
+      publicReadAccess: true,
+      removalPolicy: cdk.RemovalPolicy.DESTROY // Stateless content, safe to destroy
+    });
+    new cdk.CfnOutput(this, "Bucket", { value: siteBucket.bucketName });
+
+    const certificateArn = new acm.DnsValidatedCertificate(
+      this,
+      "SiteCertificate",
+      {
+        domainName: siteDomain,
+        hostedZone: zone,
+        region: "us-east-1" // CloudFront utilised certificates must be in Virginia
+      }
+    ).certificateArn;
+    new cdk.CfnOutput(this, "Certificate", { value: certificateArn });
+
+    const distribution = new cloudfront.CloudFrontWebDistribution(
+      this,
+      "SiteDistribution",
+      {
+        aliasConfiguration: {
+          acmCertRef: certificateArn,
+          names: [siteDomain],
+          sslMethod: cloudfront.SSLMethod.SNI,
+          securityPolicy: cloudfront.SecurityPolicyProtocol.TLS_V1_1_2016
+        },
+        originConfigs: [
+          {
+            s3OriginSource: {
+              s3BucketSource: siteBucket
+            },
+            behaviors: [{ isDefaultBehavior: true }]
+          }
+        ]
+      }
+    );
+    new cdk.CfnOutput(this, "DistributionId", {
+      value: distribution.distributionId
+    });
+
+    new route53.ARecord(this, "SiteAliasRecord", {
+      recordName: siteDomain,
+      target: route53.AddressRecordTarget.fromAlias(
+        new targets.CloudFrontTarget(distribution)
+      ),
+      zone
+    });
+
+    new s3deploy.BucketDeployment(this, "DeployWithInvalidation", {
+      sources: [s3deploy.Source.asset("../src")],
+      destinationBucket: siteBucket,
+      distribution,
+      distributionPaths: ["/*"]
+    });
+  }
+}
+
+// ## CDK Quick ref:
+// npm install -g aws-cdk
+// cdk --version
+// cdk init --language LANGUAGE [TEMPLATE]
+// cdk init --language javascript
+// cdk init --language python sample-app
+// cdk ls
+// cdk diff
+// cdk synth // Create CFN template (good for testing)
+// cdk deploy
+// cdk deploy MyStack
+// cdk destroy
+
+const app = new cdk.App();
+new PersonalSiteStack(app, "PersonalSiteStack", {
+  domainName: "bracegirdle.me",
+  env: {
+    account: process.env.CDK_DEFAULT_ACCOUNT,
+    region: process.env.CDK_DEFAULT_REGION
+  }
+});
+app.synth();
